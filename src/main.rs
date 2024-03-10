d0a14760129597194f0b294f072386571f7ce3e8
idjotherwise <ifan.johnston@gmail.com>
2024-03-10 22:19:40 +0000

    Responding with content

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,22 @@ impl Responses {
     }
 }
 
+enum ContentTypes {
+    TextPlain,
+}
+
+impl ContentTypes {
+    fn as_bytes(&self) -> &'static [u8] {
+        match self {
+            ContentTypes::TextPlain => b"Content-Type: text/plain\r\n",
+        }
+    }
+}
+
+fn to_content_length(n: usize) -> Vec<u8> {
+    format!("Content-Length: {n}\r\n\r\n").into_bytes()
+}
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -31,17 +47,34 @@ fn main() {
             Ok(mut conn) => {
                 let mut buf_reader = BufReader::new(&mut conn);
                 let mut buf_writer: String = String::new();
-                println!("Got request");
-                buf_reader.read_line(&mut buf_writer).expect("Read error");
+                let content_length = buf_reader.read_line(&mut buf_writer).expect("Read error");
                 let path = buf_writer.split(" ").skip(1).take(1).collect::<String>();
-                println!("{}", path);
+                let msg = path.split("/");
+                let first_part = msg.clone().skip(1).take(1).collect::<String>();
+                let second_part = msg.clone().skip(2).collect::<Vec<&str>>().join("/");
+                // println!("{}", first_part);
+                // println!("{}", second_part);
 
-                match path.as_str() {
-                    "/" => conn.write(Responses::Ok.as_bytes()).expect("Write error"),
-                    _ => conn
-                        .write(Responses::NotFound.as_bytes())
-                        .expect("Write error"),
-                };
+                match first_part.as_str() {
+                    "echo" => {
+                        conn.write(Responses::Ok.as_bytes())
+                            .expect("Response Status write error");
+                        conn.write(ContentTypes::TextPlain.as_bytes())
+                            .expect("Content-type write error");
+                        conn.write(&to_content_length(content_length))
+                            .expect("Content Length write error");
+                        conn.write(second_part.as_bytes())
+                            .expect("Body write error");
+                    }
+                    "" => {
+                        conn.write(Responses::Ok.as_bytes())
+                            .expect("Response Status write error");
+                    }
+                    _ => {
+                        conn.write(Responses::NotFound.as_bytes())
+                            .expect("Write error");
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
